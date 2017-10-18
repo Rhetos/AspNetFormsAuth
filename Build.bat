a09b4f467f952bee78ea5c63d4d4592ccaea035a
Bojan Antolović <bojan.antolovic@gmail.com>
2017-10-18 16:08:00 +0200

    Starting 2.1.0

--- Build.bat
+++ Build.bat
@@ -1,15 +1,17 @@
 SETLOCAL
-SET Version=2.0.0
+SET Version=2.1.0
 SET Prerelease=auto
 
 IF NOT DEFINED VisualStudioVersion CALL "%VS140COMNTOOLS%VsDevCmd.bat" || ECHO ERROR: Cannot find Visual Studio 2015, missing VS140COMNTOOLS variable. && GOTO Error0
 @ECHO ON
 
 PowerShell .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
-NuGet restore || GOTO Error0
-MsBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
-NuGet pack -o .. || GOTO Error0
-REM Updating the version of all projects back to "dev" (internal development build), to avoid spamming git history.
+WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
+NuGet restore -NonInteractive || GOTO Error0
+MSBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
+IF NOT EXIST Install md Install
+NuGet pack -o Install || GOTO Error0
+REM Updating the version of all projects back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
 PowerShell .\ChangeVersion.ps1 %Version% dev || GOTO Error0
 
 @REM ================================================
@@ -21,5 +23,5 @@ PowerShell .\ChangeVersion.ps1 %Version% dev || GOTO Error0
 :Error0
 @ECHO.
 @ECHO %~nx0 FAILED.
-@IF /I [%2] NEQ [/NOPAUSE] @PAUSE
+@IF /I [%1] NEQ [/NOPAUSE] @PAUSE
 @EXIT /B 1
