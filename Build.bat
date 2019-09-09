8afea97bf1a493f74877e53306025732be10f3a8
Bojan Antolović <bojan.antolovic@gmail.com>
2019-09-09 14:02:09 +0200

    Build script: Better Visual Studio locator.

--- Build.bat
+++ Build.bat
@@ -2,18 +2,20 @@ SETLOCAL
 SET Version=2.3.0
 SET Prerelease=auto
 
-IF NOT DEFINED VisualStudioVersion CALL "%VS140COMNTOOLS%VsDevCmd.bat" || ECHO ERROR: Cannot find Visual Studio 2015, missing VS140COMNTOOLS variable. && GOTO Error0
-@ECHO ON
+CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
 
 REM Packing the files with an older version of nuget.exe for backward compatibility (spaces in file names, https://github.com/Rhetos/Rhetos/issues/80).
 IF NOT EXIST Install MD Install
 IF NOT EXIST Install\NuGet.exe POWERSHELL (New-Object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/v4.5.1/nuget.exe', 'Install\NuGet.exe') || GOTO Error0
 
+REM Updating the build version.
 PowerShell .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
+
 Install\NuGet.exe restore -NonInteractive || GOTO Error0
 MSBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
 Install\NuGet.exe pack -OutputDirectory Install || GOTO Error0
-REM Updating the version of all projects back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
+
+REM Updating the build version back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
 PowerShell .\ChangeVersion.ps1 %Version% dev || GOTO Error0
 
 @REM ================================================
