dae0b1dee99d24d0516ae9bc8e22790ee748b79f
Bojan Antolović <bojan.antolovic@gmail.com>
2020-01-13 11:10:19 +0100

    Setting build script ExecutionPolicy to not depend on build machine configuration.

--- Build.bat
+++ Build.bat
@@ -9,14 +9,14 @@ IF NOT EXIST Install MD Install
 IF NOT EXIST Install\NuGet.exe POWERSHELL (New-Object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/v4.5.1/nuget.exe', 'Install\NuGet.exe') || GOTO Error0
 
 REM Updating the build version.
-PowerShell .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
+PowerShell -ExecutionPolicy ByPass .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
 
 Install\NuGet.exe restore -NonInteractive || GOTO Error0
 MSBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
 Install\NuGet.exe pack -OutputDirectory Install || GOTO Error0
 
 REM Updating the build version back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
-PowerShell .\ChangeVersion.ps1 %Version% dev || GOTO Error0
+PowerShell -ExecutionPolicy ByPass .\ChangeVersion.ps1 %Version% dev || GOTO Error0
 
 @REM ================================================
 
