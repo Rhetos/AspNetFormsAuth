78373038561037f26592b0acf80aeec9e2d4c9b5
Bojan Antolović <bojan.antolovic@gmail.com>
2022-02-03 22:15:52 +0100

    Consistent build scripts between Rhetos packages.

--- Build.bat
+++ Build.bat
@@ -5,20 +5,19 @@ SET Prerelease=auto
 @SET Config=%1%
 @IF [%1] == [] SET Config=Debug
 
-REM Updating the build version of all projects.
-PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
-
-WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
+REM Updating the build version.
+PowerShell -ExecutionPolicy ByPass .\tools\Build\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
 
 dotnet build --configuration %Config% -p:RhetosDeploy=false || GOTO Error0
 
-IF NOT EXIST Install\ MD Install
+IF NOT EXIST Install MD Install
 DEL /F /S /Q Install\* || GOTO Error0
 
+WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
 NuGet pack -OutputDirectory Install || GOTO Error0
 
 REM Updating the build version back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
-PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersion.ps1 %Version% dev || GOTO Error0
+PowerShell -ExecutionPolicy ByPass .\tools\Build\ChangeVersion.ps1 %Version% dev || GOTO Error0
 
 @REM ================================================
 
@@ -27,7 +26,7 @@ PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersion.ps1 %Version% dev
 @EXIT /B 0
 
 :Error0
-@PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersion.ps1 %Version% dev >nul
+@PowerShell -ExecutionPolicy ByPass .\tools\Build\ChangeVersion.ps1 %Version% dev >nul
 @ECHO.
 @ECHO %~nx0 FAILED.
 @IF /I [%1] NEQ [/NOPAUSE] @PAUSE
